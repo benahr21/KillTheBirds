41887308158f3361c27168842422c4f1fde5e2ed
Benjamin Ahr <benahr36@gmail.com>
2019-12-11 17:35:22 +0200

    Last one - out of time

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -8,12 +8,21 @@ public class Spawner : MonoBehaviour
     public GameObject[] objects;                // The prefab to be spawned.
     public float spawnTime = 6f;            // How long between each spawn.
     private Vector3 spawnPosition;
-    // Use this for initialization
+    public static int numOfBirds = 0;
     void Start()
     {
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
         InvokeRepeating("Spawn", spawnTime, spawnTime);
+        
 
+    }
+
+    void Update()
+    {
+        if (numOfBirds >= 2)
+        {
+            Application.Quit();
+        }
     }
 
     void Spawn()
@@ -23,5 +32,8 @@ public class Spawner : MonoBehaviour
         spawnPosition.z = Random.Range(0, 0);
 
         Instantiate(objects[UnityEngine.Random.Range(0, objects.Length - 1)], spawnPosition, Quaternion.identity);
+        numOfBirds++;
+        
+        
     }
 }
